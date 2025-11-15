f4d6769d16dc8574aa832e01a71f7405d5d3bcd0
Yurii <kukharenko.yura@gmail.com>
2025-11-15 18:25:54 +0200

    Added implementation of dao and service

--- src/main/java/mate/academy/springbookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/mate/academy/springbookstore/repository/impl/BookRepositoryImpl.java
@@ -3,15 +3,40 @@ package mate.academy.springbookstore.repository.impl;
 import java.util.List;
 import mate.academy.springbookstore.model.Book;
 import mate.academy.springbookstore.repository.BookRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Book save(Book book) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.persist(book);
+            session.getTransaction().commit();
+            return book;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't add book " + book, e);
+        }
     }
 
     @Override
     public List<Book> findAll() {
-        return List.of();
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> query = session.createQuery("from Book", Book.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find all books", e);
+        }
     }
 }
--- src/main/java/mate/academy/springbookstore/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/springbookstore/service/impl/BookServiceImpl.java
@@ -2,16 +2,23 @@ package mate.academy.springbookstore.service.impl;
 
 import java.util.List;
 import mate.academy.springbookstore.model.Book;
+import mate.academy.springbookstore.repository.BookRepository;
 import mate.academy.springbookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
     @Override
     public Book save(Book book) {
-        return null;
+        return bookRepository.save(book);
     }
 
     @Override
     public List<Book> findAll() {
-        return List.of();
+        return bookRepository.findAll();
     }
 }
