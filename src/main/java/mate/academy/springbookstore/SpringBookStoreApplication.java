f4776275f9f66fbecdf51f9f237823177aaab80c
Yurii <kukharenko.yura@gmail.com>
2025-11-15 18:27:56 +0200

    Checkstyle issues fixed.

--- src/main/java/mate/academy/springbookstore/SpringBookStoreApplication.java
+++ src/main/java/mate/academy/springbookstore/SpringBookStoreApplication.java
@@ -1,15 +1,13 @@
 package mate.academy.springbookstore;
 
+import java.math.BigDecimal;
 import mate.academy.springbookstore.model.Book;
-import mate.academy.springbookstore.repository.impl.BookRepositoryImpl;
 import mate.academy.springbookstore.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication
 public class SpringBookStoreApplication {
