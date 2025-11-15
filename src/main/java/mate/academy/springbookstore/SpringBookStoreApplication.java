dc21a447df5f91e50fc5a7848191738b9ea5e3c0
Yurii <kukharenko.yura@gmail.com>
2025-11-15 15:16:48 +0200

    Commiting infrastructure changes

---
+++ src/main/java/mate/academy/springbookstore/SpringBookStoreApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.springbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBookStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/springbookstore/SpringBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
