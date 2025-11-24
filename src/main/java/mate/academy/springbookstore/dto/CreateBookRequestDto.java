68816f28f5b43f107be2363b82a9ded411982896
Yurii <kukharenko.yura@gmail.com>
2025-11-24 20:27:33 +0200

    Added CustomGlobalExceptionHandler. Implemented validation for request dto.

--- src/main/java/mate/academy/springbookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/springbookstore/dto/CreateBookRequestDto.java
@@ -1,5 +1,10 @@
 package mate.academy.springbookstore.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,10 +14,21 @@ import lombok.Setter;
 @Getter
 @RequiredArgsConstructor
 public class CreateBookRequestDto {
+    @NotBlank(message = "{book.request.title.notblank}")
+    @Size(max = 255, message = "{book.request.title.size}")
     private String title;
+    @NotBlank(message = "{book.request.author.notblank}")
+    @Size(max = 255, message = "{book.request.author.size}")
     private String author;
+    @NotBlank(message = "{book.request.isbn.notblank}")
+    @Pattern(regexp = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$",
+            message = "{book.request.isbn.format}")
     private String isbn;
+    @NotNull(message = "{book.request.price.notnull}")
+    @DecimalMin(value = "0.01", message = "{book.request.price.min}")
     private BigDecimal price;
+    @Size(max = 666, message = "{book.request.description.max}")
     private String description;
+    @Size(max = 255, message = "{book.request.coverImage.max}")
     private String coverImage;
 }
