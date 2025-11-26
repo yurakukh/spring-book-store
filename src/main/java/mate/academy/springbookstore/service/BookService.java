8a7267b8c991c0de2992ea1523fbb19af74ea7a3
Yurii <kukharenko.yura@gmail.com>
2025-11-26 22:07:35 +0200

    Added pagination and sorting to project. Added swagger

--- src/main/java/mate/academy/springbookstore/service/BookService.java
+++ src/main/java/mate/academy/springbookstore/service/BookService.java
@@ -1,13 +1,14 @@
 package mate.academy.springbookstore.service;
 
-import java.util.List;
 import mate.academy.springbookstore.dto.BookDto;
 import mate.academy.springbookstore.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    Page<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
