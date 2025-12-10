f54d383e723534b533ddd0bbe48e8ab4227a3737
Yurii <kukharenko.yura@gmail.com>
2025-12-10 11:17:03 +0200

    Minor fix

--- src/main/java/mate/academy/springbookstore/config/SecurityConfig.java
+++ src/main/java/mate/academy/springbookstore/config/SecurityConfig.java
@@ -47,8 +47,12 @@ public class SecurityConfig {
                                 .authenticated()
                 )
                 .httpBasic(withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .userDetailsService(userDetailsService)
                 .build();
     }
