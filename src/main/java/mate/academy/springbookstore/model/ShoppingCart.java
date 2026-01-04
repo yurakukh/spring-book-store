aced2818429339e99b49bf241d15ee5e1e8c220c
Yurii <kukharenko.yura@gmail.com>
2026-01-04 17:04:55 +0200

    Resolved minor issues

--- src/main/java/mate/academy/springbookstore/model/ShoppingCart.java
+++ src/main/java/mate/academy/springbookstore/model/ShoppingCart.java
@@ -3,8 +3,6 @@ package mate.academy.springbookstore.model;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
