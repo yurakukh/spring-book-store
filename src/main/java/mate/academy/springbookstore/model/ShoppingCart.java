5951e3428436ac68067d122b2aef83dee134c393
Yurii <kukharenko.yura@gmail.com>
2026-01-04 17:03:21 +0200

    Resolved minor issues

--- src/main/java/mate/academy/springbookstore/model/ShoppingCart.java
+++ src/main/java/mate/academy/springbookstore/model/ShoppingCart.java
@@ -22,7 +22,6 @@ import lombok.Setter;
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @MapsId
