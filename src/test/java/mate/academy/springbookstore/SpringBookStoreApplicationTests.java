dce62de57b7694db0b40e19914e0beee7271320f
Yurii <kukharenko.yura@gmail.com>
2026-01-09 17:02:49 +0200

    Added docker

--- src/test/java/mate/academy/springbookstore/SpringBookStoreApplicationTests.java
+++ src/test/java/mate/academy/springbookstore/SpringBookStoreApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBookStoreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
